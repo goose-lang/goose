3f02048ba12206305256c194559f38898d631452
Tej Chajed <tchajed@mit.edu>
2019-02-09 12:59:20 -0500

    Process a basic type declaration

--- main.go
+++ main.go
@@ -15,62 +15,141 @@ import (
 // issue was encountered in a uniform way.
 type ErrorReporter struct{}
 
-func (r ErrorReporter) prefixed(prefix, msg string) {
-	panic(fmt.Errorf("%s: %s", prefix, msg))
+func (r ErrorReporter) prefixed(prefix, msg string, args ...interface{}) {
+	formatted := fmt.Sprintf(msg, args...)
+	panic(fmt.Errorf("%s: %s", prefix, formatted))
 }
 
 // Docs reports a situation that I thought was impossible from reading the documentation.
-func (r ErrorReporter) Docs(msg string) {
-	r.prefixed("impossible(docs)", msg)
+func (r ErrorReporter) Docs(msg string, args ...interface{}) {
+	r.prefixed("impossible(docs)", msg, args...)
 }
 
 // NoExample reports a situation I thought was impossible because I couldn't
 // think of how to do it in Go.
-func (r ErrorReporter) NoExample(msg string) {
-	r.prefixed("impossible(no-examples)", msg)
+func (r ErrorReporter) NoExample(msg string, args ...interface{}) {
+	r.prefixed("impossible(no-examples)", msg, args...)
 }
 
 // FutureWork reports something we could theoretically handle but probably
 // won't.
-func (r ErrorReporter) FutureWork(msg string) {
-	r.prefixed("future", msg)
+func (r ErrorReporter) FutureWork(msg string, args ...interface{}) {
+	r.prefixed("future", msg, args...)
 }
 
 // Todo reports a situation that is intended to be handled but we haven't gotten
 // around to.
-func (r ErrorReporter) Todo(msg string) {
-	r.prefixed("todo", msg)
+func (r ErrorReporter) Todo(msg string, args ...interface{}) {
+	r.prefixed("todo", msg, args...)
 }
 
 // Unhandled reports something intentionally unhandled (the code should not use
 // this feature).
-func (r ErrorReporter) Unhandled(msg string) {
-	r.prefixed("unhandled", msg)
+func (r ErrorReporter) Unhandled(msg string, args ...interface{}) {
+	r.prefixed("unhandled", msg, args...)
 }
 
 var error ErrorReporter
 
+// TypeDecl is a name:type declaration (for a struct)
+type TypeDecl struct {
+	Name string
+	Type string
+}
+
+// StructType is a Coq record for a Go struct
+type StructType struct {
+	Name   string
+	Fields []TypeDecl
+}
+
+func isIdent(e ast.Expr, s string) bool {
+	switch e := e.(type) {
+	case *ast.Ident:
+		return e.Name == s
+	default:
+		return false
+	}
+}
+
+func mapType(e *ast.MapType) string {
+	switch k := e.Key.(type) {
+	case *ast.Ident:
+		if k.Name == "uint64" {
+			return fmt.Sprintf("HashTable(%s)", typeToString(e.Value))
+		}
+	default:
+	}
+	error.Unhandled("maps must be from uint64")
+	return "<bad hashtable>"
+}
+
+func selectorExprType(e *ast.SelectorExpr) string {
+	if isIdent(e.X, "filesys") &&
+		e.Sel != nil && e.Sel.Name == "File" {
+		return "Fd"
+	}
+	error.Unhandled("selector for unknown type %s", spew.Sdump(e))
+	return "<selector expr>"
+}
+
+func typeToString(e ast.Expr) string {
+	switch e := e.(type) {
+	case *ast.Ident:
+		return e.Name
+	case *ast.MapType:
+		return mapType(e)
+	case *ast.SelectorExpr:
+		return selectorExprType(e)
+	default:
+	}
+	error.NoExample("unexpected type expr %s", spew.Sdump(e))
+	return "<type>"
+}
+
+func structDecl(name string, structTy *ast.StructType) StructType {
+	ty := StructType{Name: name}
+	for _, f := range structTy.Fields.List {
+		if len(f.Names) > 1 {
+			error.FutureWork("multiple fields for same type (split them up)")
+		}
+		fieldDecl := TypeDecl{
+			Name: f.Names[0].Name,
+			Type: typeToString(f.Type),
+		}
+		ty.Fields = append(ty.Fields, fieldDecl)
+	}
+	return ty
+}
+
 func traceDecls(ds []ast.Decl) {
 	for _, d := range ds {
-		switch v := d.(type) {
+		switch d := d.(type) {
 		case *ast.FuncDecl:
-			spew.Printf("func %s\n%#v\n", v.Name.Name, v)
+			spew.Printf("func %s\n%#v", d.Name.Name, d)
 		case *ast.GenDecl:
-			switch v.Tok {
+			switch d.Tok {
 			case token.IMPORT, token.CONST, token.VAR:
 				continue
 			case token.TYPE:
-				if len(v.Specs) > 1 {
+				if len(d.Specs) > 1 {
 					error.NoExample("multiple specs in a type decl")
 				}
-				s := v.Specs[0].(*ast.TypeSpec)
-				spew.Printf("type %s\n%#v\n", s.Name.Name, s)
+				spec := d.Specs[0].(*ast.TypeSpec)
+				if structTy, ok := spec.Type.(*ast.StructType); ok {
+					ty := structDecl(spec.Name.Name, structTy)
+					spew.Printf("type %s\n%#v\n", spec.Name.Name, structTy)
+					fmt.Printf("%+v\n", ty)
+				} else {
+					error.Unhandled("non-struct type %s", spec.Name.Name)
+				}
 			default:
-				error.Docs("unknown gendecl token type")
+				error.Docs("unknown gendecl token type for %+v", d)
 			}
 		default:
-			error.NoExample("top-level decl")
+			error.NoExample("top-level decl %s", spew.Sdump(d))
 		}
+		fmt.Printf("\n\n")
 	}
 }
 
