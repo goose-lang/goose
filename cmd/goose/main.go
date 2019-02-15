a3bc585e39fcfd9129d03d4ec2a18ddc14b90c9e
Tej Chajed <tchajed@mit.edu>
2019-02-15 10:49:03 -0500

    Support directly writing to a file
    Avoids overwriting the file on error

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -13,6 +13,10 @@ func main() {
 	flag.BoolVar(&config.AddSourceFileComments, "source-comments", false,
 		"add comments indicating Go source code location for each top-level declaration")
 
+	var outFile string
+	flag.StringVar(&outFile, "out", "-",
+		"file to output to (use '-' for stdout)")
+
 	flag.Parse()
 	if flag.NArg() != 1 {
 		fmt.Fprintln(os.Stderr, "Usage: goose <path to source dir>")
@@ -25,5 +29,16 @@ func main() {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
-	f.Write(os.Stdout)
+	if outFile == "-" {
+		f.Write(os.Stdout)
+	} else {
+		out, err := os.Create(outFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			fmt.Fprintln(os.Stderr, "could not write output")
+			os.Exit(1)
+		}
+		defer out.Close()
+		f.Write(out)
+	}
 }
