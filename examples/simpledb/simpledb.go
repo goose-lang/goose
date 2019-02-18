c1c392a8d2c9d5ec27addc7caf9292033e46915d
Tej Chajed <tchajed@mit.edu>
2019-02-17 19:17:54 -0500

    Update simpledb example
    This is now a feature-complete implementation

--- examples/simpledb/simpledb.go
+++ examples/simpledb/simpledb.go
@@ -295,3 +295,195 @@ func Write(db Database, k uint64, v []byte) {
 	buf[k] = v
 	db.bufferL.Unlock()
 }
+
+func freshTable(p string) string {
+	if p == "table.0" {
+		return "table.1"
+	}
+	if p == "table.1" {
+		return "table.0"
+	}
+	// note that this only works if we guarantee the table is always
+	// either table.0 or table.1
+	return p
+}
+
+func tablePutBuffer(w tableWriter, buf map[uint64][]byte) {
+	for k, v := range buf {
+		tablePut(w, k, v)
+	}
+}
+
+// add all of table t to the table w being created; skip any keys in the (read)
+// buffer b since those writes overwrite old ones
+func tablePutOldTable(w tableWriter, t Table, b map[uint64][]byte) {
+	for buf := (lazyFileBuf{offset: 0, next: nil}); ; {
+		e, l := DecodeEntry(buf.next)
+		if l > 0 {
+			_, ok := b[e.Key]
+			// only copy the key if it wasn't overwritten in the buffer
+			// (this compacts overall storage when keys are overwritten)
+			if !ok {
+				tablePut(w, e.Key, e.Value)
+			}
+			buf = lazyFileBuf{offset: buf.offset + l, next: buf.next[l:]}
+			continue
+		} else {
+			p := filesys.ReadAt(t.File, buf.offset, 4096)
+			if len(p) == 0 {
+				break
+			} else {
+				newBuf := append(buf.next, p...)
+				buf = lazyFileBuf{
+					offset: buf.offset,
+					next:   newBuf,
+				}
+				continue
+			}
+		}
+	}
+}
+
+// Build a new shadow table that incorporates the current table and a
+// (write) buffer wbuf.
+//
+// Assumes all the appropriate locks have been taken.
+//
+// Returns the old table and new table.
+func constructNewTable(db Database, wbuf map[uint64][]byte) (Table, Table) {
+	oldName := *db.tableName
+	name := freshTable(oldName)
+	w := newTableWriter(name)
+	oldTable := *db.table
+	// add old writes (using the buffer to skip new writes)
+	tablePutOldTable(w, oldTable, wbuf)
+	// add buffered writes
+	tablePutBuffer(w, wbuf)
+	newTable := tableWriterClose(w)
+	return oldTable, newTable
+}
+
+func Compact(db Database) {
+	db.compactionL.Lock()
+
+	// first, snapshot the buffered writes that will go into this table.
+	db.bufferL.Lock()
+	buf := *db.wbuffer
+	emptyWbuffer := make(map[uint64][]byte)
+	*db.wbuffer = emptyWbuffer
+	*db.rbuffer = buf
+	db.bufferL.Unlock()
+
+	// next, construct the new table
+	// NOTE: it's weird to acquire this table lock:
+	//
+	//	- what happens between tableL.RUnlock() and tableL.Lock()? a reader
+	// would be fine since the old table is still valid, and the compaction
+	// log prevents another writer from suddenly installing a new table
+	//
+	// - given this reasoning, why lock the tableL at all? it feels like the
+	// compactionL also implicitly acquires a read-lock on the tableL, since it
+	// guarantees only the compaction itself could be messing with the table,
+	// which it won't do till later in this function
+	db.tableL.RLock()
+	oldTableName := *db.tableName
+	oldTable, t := constructNewTable(db, buf)
+	newTable := freshTable(oldTableName)
+	db.tableL.RUnlock()
+
+	// next, install it (persistently and in-memory)
+	db.tableL.Lock()
+	*db.table = t
+	*db.tableName = newTable
+	manifestData := []byte(newTable)
+	filesys.AtomicCreate("manifest", manifestData)
+	CloseTable(oldTable)
+	filesys.Delete(oldTableName)
+	// note that we don't need to remove the rbuffer (it's just a cache for
+	// the part of the table we just persisted)
+	db.tableL.Unlock()
+
+	db.compactionL.Unlock()
+}
+
+func recoverManifest() string {
+	f := filesys.Open("manifest")
+	// need to know that table names are less than 4096 bytes
+	// (eventually we'll probably restrict ReadAt to read at most 4096 bytes
+	//  at a time due to it being atomic,
+	//  so checking the size of the file won't help,
+	//  and there's really no need for a loop here)
+	manifestData := filesys.ReadAt(f, 0, 4096)
+	tableName := string(manifestData)
+	filesys.Close(f)
+	return tableName
+}
+
+// delete 'name' if it isn't tableName or "manifest"
+func deleteOtherFile(name string, tableName string) {
+	if name == tableName {
+		return
+	}
+	if name == "manifest" {
+		return
+	}
+	filesys.Delete(name)
+}
+
+func deleteOtherFiles(tableName string) {
+	files := filesys.List()
+	nfiles := uint64(len(files))
+	for i := uint64(0); ; {
+		if i == nfiles {
+			break
+		}
+		name := files[i]
+		deleteOtherFile(name, tableName)
+		i = i + 1
+		continue
+	}
+}
+
+func Recover() Database {
+	tableName := recoverManifest()
+	table := RecoverTable(tableName)
+	tableRef := new(Table)
+	*tableRef = table
+	tableNameRef := new(string)
+	*tableNameRef = tableName
+
+	deleteOtherFiles(tableName)
+
+	wbuffer := makeValueBuffer()
+	rbuffer := makeValueBuffer()
+	bufferL := new(sync.RWMutex)
+	tableL := new(sync.RWMutex)
+	compactionL := new(sync.RWMutex)
+
+	return Database{
+		wbuffer:     wbuffer,
+		rbuffer:     rbuffer,
+		bufferL:     bufferL,
+		table:       tableRef,
+		tableName:   tableNameRef,
+		tableL:      tableL,
+		compactionL: compactionL,
+	}
+}
+
+// immediate shutdown; like a crash, but cleanly close all files
+func Shutdown(db Database) {
+	db.bufferL.Lock()
+	db.compactionL.Lock()
+
+	t := *db.table
+	CloseTable(t)
+
+	db.compactionL.Unlock()
+	db.bufferL.Unlock()
+}
+
+func Close(db Database) {
+	Compact(db)
+	Shutdown(db)
+}
