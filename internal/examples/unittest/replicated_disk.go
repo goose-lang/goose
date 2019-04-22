df8385001788fa2d0a78a208aae36f94bfe04e84
Tej Chajed <tchajed@mit.edu>
2019-04-22 11:12:48 -0400

    Add replicated disk read and write

--- internal/examples/unittest/replicated_disk.go
+++ internal/examples/unittest/replicated_disk.go
@@ -22,6 +22,30 @@ func TwoDiskRead(diskId uint64, a uint64) (Block, bool) {
 // base layer
 func TwoDiskLock(a uint64) {}
 
+// TwoDiskUnlock is a dummy function to represent unlocking an address in the
+// base layer
+func TwoDiskUnlock(a uint64) {}
+
+func ReplicatedDiskRead(a uint64) Block {
+	TwoDiskLock(a)
+	v, ok := TwoDiskRead(Disk1, a)
+	if ok {
+		TwoDiskUnlock(a)
+		return v
+	}
+	v2, _ := TwoDiskRead(Disk2, a)
+	// we assume both disks cannot fail
+	TwoDiskUnlock(a)
+	return v2
+}
+
+func ReplicatedDiskWrite(a uint64, v Block) {
+	TwoDiskLock(a)
+	TwoDiskWrite(Disk1, a, v)
+	TwoDiskWrite(Disk2, a, v)
+	TwoDiskUnlock(a)
+}
+
 func ReplicatedDiskRecover() {
 	for a := uint64(0); ; {
 		if a > DiskSize {
