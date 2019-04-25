1d0224456728cd0a4291059c80a49cefb4ad1fc5
Tej Chajed <tchajed@mit.edu>
2019-04-25 11:46:47 -0400

    Revert back to ordinary AtomicCreate

--- machine/filesys/dir.go
+++ machine/filesys/dir.go
@@ -83,6 +83,31 @@ func (fs DirFs) Delete(dir, fname string) {
 	}
 }
 
+func (fs DirFs) AtomicCreate(dir, fname string, data []byte) {
+	tmpFile := fname + ".tmp"
+	fd, err := unix.Openat(fs.rootFd, tmpFile,
+		unix.O_CREAT|unix.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer unix.Close(fd)
+	for len(data) > 0 {
+		n, err := unix.Write(fd, data)
+		if err != nil {
+			panic(err)
+		}
+		data = data[n:]
+	}
+	err = unix.Fsync(fd)
+	if err != nil {
+		panic(err)
+	}
+	err = unix.Renameat(fs.rootFd, tmpFile, fs.rootFd, path.Join(dir, fname))
+	if err != nil {
+		panic(err)
+	}
+}
+
 func (fs DirFs) Link(oldDir, oldName, newDir, newName string) bool {
 	err := unix.Linkat(fs.rootFd, path.Join(oldDir, oldName),
 		fs.rootFd, path.Join(newDir, newName),
