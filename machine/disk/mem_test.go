7eae97ca04af13267bb632aa0060810ad2aff0a4
Tej Chajed <tchajed@mit.edu>
2021-05-12 08:27:17 -0400

    Limit memory usage of benchmark

--- machine/disk/mem_test.go
+++ machine/disk/mem_test.go
@@ -30,17 +30,18 @@ func BenchmarkMemWrite10(b *testing.B) {
 
 func BenchmarkSequentialWrite(b *testing.B) {
 	zero := make([]byte, 4096)
-	d := disk.NewMemDisk(uint64(b.N))
+	size := uint64(10000)
+	d := disk.NewMemDisk(size)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		d.Write(uint64(i), zero)
+		d.Write(uint64(i)%size, zero)
 	}
 	b.ReportMetric(float64(b.N)*4/1024, "MB/s")
 }
 
 func BenchmarkSequentialRead(b *testing.B) {
 	size := 10000
-	d := disk.NewMemDisk(10000)
+	d := disk.NewMemDisk(size)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		d.Read(uint64(i % size))
