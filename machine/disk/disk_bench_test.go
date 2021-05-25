14c378ed6fb6909713ea3c0b7b6f750410d6763a
Tej Chajed <tchajed@mit.edu>
2021-05-25 11:29:01 -0400

    Expand disk benchmarks

--- machine/disk/disk_bench_test.go
+++ machine/disk/disk_bench_test.go
@@ -1,6 +1,7 @@
 package disk_test
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -9,74 +10,122 @@ import (
 	"github.com/tchajed/goose/machine/disk"
 )
 
+var diskFile = flag.String("disk", "", "path to file to use for disk-based benchmark")
+
 func createMemDisk() disk.MemDisk {
 	return disk.NewMemDisk(10000)
 }
 
-func createTmpDisk() (string, disk.FileDisk) {
+func createTmpPath() string {
 	num := rand.Intn(1000)
-	path := fmt.Sprintf("/tmp/disk-%d.img", num)
+	return fmt.Sprintf("/tmp/disk-%d.img", num)
+}
 
+func createFileDisk(path string) disk.FileDisk {
 	d, err := disk.NewFileDisk(path, 10000)
 	if err != nil {
 		panic(err)
 	}
-	return path, d
+	return d
 }
 
-func destroyTmpDisk(path string, d disk.FileDisk) {
+func destroyFileDisk(path string, d disk.FileDisk) {
 	d.Close()
-	os.Remove(path)
-}
-
-func benchRead10(b *testing.B, d disk.Disk) {
-	size := d.Size()
-	b.ResetTimer()
-	iter := uint64(0)
-	for i := 0; i < b.N; i++ {
-		for j := uint64(0); j < 10; j++ {
-			d.Read(iter % size)
-			iter++
-		}
+	f, err := os.Stat(path)
+	if err != nil {
+		panic(err)
 	}
-	b.ReportMetric(float64(iter)*4/1024, "MB/s")
+	if f.Mode().IsRegular() {
+		os.Remove(path)
+	}
 }
 
-func BenchmarkMemRead10(b *testing.B) {
-	d := createMemDisk()
-	benchRead10(b, d)
+// Run benchmark f on several disks, as sub-benchmarks
+func benchmarkOnDisks(b *testing.B, f func(b *testing.B, d disk.Disk)) {
+	b.Helper()
+	b.Run("Mem", func(b *testing.B) {
+		d := createMemDisk()
+		f(b, d)
+	})
+	b.Run("TmpFile", func(b *testing.B) {
+		path := createTmpPath()
+		d := createFileDisk(path)
+		defer destroyFileDisk(path, d)
+		f(b, d)
+		b.StopTimer()
+	})
+	if *diskFile != "" {
+		b.Run("DiskFile", func(b *testing.B) {
+			path := *diskFile
+			d := createFileDisk(path)
+			defer destroyFileDisk(path, d)
+			f(b, d)
+			b.StopTimer()
+		})
+	}
 }
 
-func BenchmarkTmpFileRead10(b *testing.B) {
-	path, d := createTmpDisk()
-	benchRead10(b, d)
-	b.StopTimer()
-	destroyTmpDisk(path, d)
+func BenchmarkRead10(b *testing.B) {
+	benchmarkOnDisks(b,
+		func(b *testing.B, d disk.Disk) {
+			size := d.Size()
+			b.ResetTimer()
+			iter := uint64(0)
+			for i := 0; i < b.N; i++ {
+				for j := uint64(0); j < 10; j++ {
+					d.Read(iter % size)
+					iter++
+				}
+			}
+			b.ReportMetric(float64(iter)*4/1024, "MB/s")
+		})
 }
 
-func benchWrite10(b *testing.B, d disk.Disk) {
-	size := d.Size()
-	data := make([]byte, 4096)
-	copy(data, []byte{1, 2, 3, 4})
-	b.ResetTimer()
-	iter := uint64(0)
-	for i := 0; i < b.N; i++ {
-		for j := uint64(0); j < 10; j++ {
+func BenchmarkWrite10(b *testing.B) {
+	benchmarkOnDisks(b, func(b *testing.B, d disk.Disk) {
+		size := d.Size()
+		data := make([]byte, 4096)
+		copy(data, []byte{1, 2, 3, 4})
+		b.ResetTimer()
+		iter := uint64(0)
+		for i := 0; i < b.N; i++ {
+			for j := uint64(0); j < 10; j++ {
+				d.Write(iter%size, data)
+				iter++
+			}
+		}
+		b.ReportMetric(float64(iter)*4/1024, "MB/s")
+	})
+}
+
+func BenchmarkBarrierOneWrite(b *testing.B) {
+	benchmarkOnDisks(b, func(b *testing.B, d disk.Disk) {
+		size := d.Size()
+		data := make([]byte, 4096)
+		copy(data, []byte{1, 2, 3, 4})
+		b.ResetTimer()
+		iter := uint64(0)
+		for i := 0; i < b.N; i++ {
 			d.Write(iter%size, data)
+			d.Barrier()
 			iter++
 		}
-	}
-	b.ReportMetric(float64(iter)*4/1024, "MB/s")
+	})
 }
 
-func BenchmarkMemWrite10(b *testing.B) {
-	d := createMemDisk()
-	benchWrite10(b, d)
-}
-
-func BenchmarkTmpFileWrite10(b *testing.B) {
-	path, d := createTmpDisk()
-	benchWrite10(b, d)
-	b.StopTimer()
-	destroyTmpDisk(path, d)
+func BenchmarkBarrierWrite10(b *testing.B) {
+	benchmarkOnDisks(b, func(b *testing.B, d disk.Disk) {
+		size := d.Size()
+		data := make([]byte, 4096)
+		copy(data, []byte{1, 2, 3, 4})
+		b.ResetTimer()
+		iter := uint64(0)
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < 10; j++ {
+				d.Write(iter%size, data)
+			}
+			d.Barrier()
+			iter++
+		}
+	})
 }
