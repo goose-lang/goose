9253fbc1031fa59552ed0c2f08fb62bf629f0386
Tej Chajed <tchajed@mit.edu>
2022-01-29 10:19:46 -0600

    Fix ground-truth error position in badif2

--- testdata/negative-tests/badif2.go
+++ testdata/negative-tests/badif2.go
@@ -4,8 +4,8 @@ func Skip() {}
 
 func BadIf2(i uint64) {
 	if i == 0 {
-		if true { // ERROR return in unsupported position
-			return
+		if true {
+			return // ERROR return in unsupported position
 		} else {
 			Skip()
 		}
