cc711a8c63c14d97a084c7f992e8f8bdc648fa21
Upamanyu Sharma <upamanyu@mit.edu>
2024-05-06 13:50:35 -0400

    Avoid unfolding type alias when referring to it in another package

--- types.go
+++ types.go
@@ -43,6 +43,11 @@ func (ctx Ctx) mapType(e *ast.MapType) coq.MapType {
 }
 
 func (ctx Ctx) selectorExprType(e *ast.SelectorExpr) coq.Expr {
+	pkgIdent, ok := e.X.(*ast.Ident)
+	if !ok {
+		ctx.nope(e, "selector expr type where package name is not identifier")
+	}
+
 	if isIdent(e.X, "filesys") && isIdent(e.Sel, "File") {
 		return coq.TypeIdent("fileT")
 	}
@@ -53,7 +58,17 @@ func (ctx Ctx) selectorExprType(e *ast.SelectorExpr) coq.Expr {
 		(isIdent(e.Sel, "Cond") || isIdent(e.Sel, "Mutex")) {
 		ctx.unsupported(e, "%s without pointer indirection", ctx.printGo(e))
 	}
-	return ctx.coqTypeOfType(e, ctx.typeOf(e))
+
+	// FIXME(HACK): this is to make sure `pkg.Foo` doesn't get unfolded to its
+	// referrring type.
+	t := ctx.typeOf(e)
+	if isProphId(t) {
+		return coq.TypeIdent("ProphIdT")
+	}
+	if _, ok := ctx.getStructInfo(t); ok {
+		return coq.StructName(pkgIdent.Name + "." + e.Sel.Name)
+	}
+	return coq.TypeIdent(pkgIdent.Name + "." + e.Sel.Name)
 }
 
 func (ctx Ctx) coqTypeOfType(n ast.Node, t types.Type) coq.Type {
