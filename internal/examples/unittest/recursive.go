c52ad991c839ce50b1544f4e8985ecd9274e5d16
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-05-24 17:05:23 -0400

    recursive function support

--- internal/examples/unittest/recursive.go
+++ internal/examples/unittest/recursive.go
@@ -1,10 +1,25 @@
 package unittest
 
-/*
-func recur(n uint64) uint64 {
-	if n == 0 {
-		return 0
+// Should be able to detect even if in nested scopes.
+func recur1() {
+	if true {
+		recur1()
 	}
-	return n + recur(n-1)
 }
-*/
+
+type recurStruct1 struct{}
+
+func (s *recurStruct1) recur2() {
+	s.recur2()
+}
+
+type recurStruct2 struct {
+	lam func()
+}
+
+func (s *recurStruct2) recur3() {
+	s.lam = func() {
+		s.lam()
+	}
+	s.lam()
+}
