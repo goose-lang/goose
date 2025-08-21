aa8d36b7b2df222e6a1976da27285ca42ccf066f
Upamanyu Sharma <upamanyu@mit.edu>
2025-08-21 16:22:35 -0400

    Fix

--- goose.go
+++ goose.go
@@ -804,15 +804,11 @@ func (ctx *Ctx) selectorExpr(e *ast.SelectorExpr) glang.Expr {
 		} else if f, ok := ctx.info.ObjectOf(e.Sel).(*types.Func); ok {
 			// If there are type arguments, we must pass them
 			typeArgs := ctx.info.Instances[e.Sel].TypeArgs
-			return ctx.handleImplicitConversion(e,
-				ctx.info.TypeOf(e.Sel),
-				ctx.info.TypeOf(e),
-				glang.NewCallExpr(
-					glang.GallinaVerbatim("func_call"),
-					glang.StringVal{Value: ctx.gallinaIdent(f.Pkg().Name() + "." + f.Name())},
-				).Append(
-					typesToExprs(ctx.convertTypeArgsToGlang(nil, typeArgs))...,
-				),
+			return glang.NewCallExpr(
+				glang.GallinaVerbatim("func_call"),
+				glang.StringVal{Value: ctx.gallinaIdent(f.Pkg().Name() + "." + f.Name())},
+			).Append(
+				typesToExprs(ctx.convertTypeArgsToGlang(nil, typeArgs))...,
 			)
 		} else {
 			return ctx.handleImplicitConversion(e,
@@ -2102,7 +2098,8 @@ func (ctx *Ctx) handleImplicitConversion(n locatable, from, to types.Type, e gla
 			if types.AssignableTo(fromUnder, toUnder) {
 				return e
 			} else {
-				ctx.unsupported(n, "function conversion from %s to %s", from, to)
+				panic("test")
+				// ctx.unsupported(n, "function conversion from %s to %s", from, to)
 			}
 		}
 	}
