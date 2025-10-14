fb98cad02c155930fe95b766d904ed50cff9d29d
Tej Chajed <chajed@wisc.edu>
2025-10-14 10:35:38 -0500

    Add model for channel iteration
    This is intended as documentation and for use in tests - for now, the
    model we use for translation needs to take a loop body that produces
    special GoLang break/continue values.

--- model/channel/channel.go
+++ model/channel/channel.go
@@ -276,6 +276,27 @@ func (c *Channel[T]) Cap() uint64 {
 	return c.cap
 }
 
+// c.Iter() returns an iterator that models a for range loop over the channel.
+func (c *Channel[T]) Iter() func(yield func(T) bool) {
+	return func(yield func(T) bool) {
+		for {
+			selected, v, ok := c.TryReceive(false)
+			// no progress this iteration, try again
+			if !selected {
+				continue
+			}
+			// iteration is done
+			if !ok {
+				return
+			}
+			if !yield(v) {
+				// early exit
+				return
+			}
+		}
+	}
+}
+
 // The code below models select statements in a similar way to the reflect package's
 // dynamic select statements. See unit tests in channel_test.go for examples of
 // the intended translation.
