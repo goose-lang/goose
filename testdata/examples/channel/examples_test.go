51e26d3ea6c4f7ac6525697d05456d8eafbd4238
Tej Chajed <chajed@wisc.edu>
2025-10-31 15:15:08 -0500

    Add a higher-order channel example
    Loosely based on Effective Go's channels of channels example, but also
    with a function passed in the request.

--- testdata/examples/channel/examples_test.go
+++ testdata/examples/channel/examples_test.go
@@ -1,6 +1,9 @@
 package chan_spec_raw_examples
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestAll(t *testing.T) {
 	TestHelloWorldSync()
@@ -9,4 +12,12 @@ func TestAll(t *testing.T) {
 	TestFibConsumer()
 	TestSelectNbNoPanic()
 	TestSelectReadyCaseNoPanic()
+}
+
+func TestHigherOrderExample(t *testing.T) {
+	responses := HigherOrderExample()
+	expected := []string{"hello world", "HELLO", "world"}
+	if !strings.EqualFold(strings.Join(responses, ""), strings.Join(expected, "")) {
+		t.Errorf("Expected %v, got %v", expected, responses)
+	}
 }
