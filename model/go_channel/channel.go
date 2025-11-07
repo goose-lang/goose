0bde7840f726e5ce5434b6f536682e4f1cedb987
Tej Chajed <chajed@wisc.edu>
2025-11-07 14:19:45 -0600

    Port all channel tests to Go channels
    Written mostly with Claude Sonnet 4.5

--- model/go_channel/channel.go
+++ model/go_channel/channel.go
@@ -2,3 +2,133 @@
 //
 // This is only used for testing the model - it makes it easy to use the same example with the model and Go channels.
 package go_channel
+
+type Channel[T any] struct {
+	ch chan T
+}
+
+func NewChannel[T any](cap int) *Channel[T] {
+	return &Channel[T]{
+		ch: make(chan T, cap),
+	}
+}
+
+// Non-Blocking send operation for select statements. Blocking send and blocking select
+// statements simply call this in a for loop until it returns true.
+func (c *Channel[T]) TrySend(val T, blocking bool) bool {
+	if blocking {
+		c.ch <- val
+		return true
+	}
+	select {
+	case c.ch <- val:
+		return true
+	default:
+		return false
+	}
+}
+
+// c.Send(val)
+//
+// is equivalent to:
+//
+// c <- val
+func (c *Channel[T]) Send(v T) {
+	if c == nil {
+		for {
+		}
+	}
+	c.ch <- v
+}
+
+// Non-blocking receive function used for select statements.
+// The blocking parameter here is used to determine whether or not we will make an offer to a
+// waiting sender. If true, we will make an offer since blocking receive is modeled as a for loop
+// around nonblocking TryReceive. If false, we don't make an offer since we don't need to match
+// with another non-blocking send.
+func (c *Channel[T]) TryReceive(blocking bool) (bool, T, bool) {
+	if blocking {
+		v, ok := <-c.ch
+		return true, v, ok
+	}
+	select {
+	case v, ok := <-c.ch:
+		return true, v, ok
+	default:
+		var zero T
+		return false, zero, true
+	}
+}
+
+// Equivalent to:
+// value, ok := <-c
+// Notably, this requires the user to consume the ok bool which is not actually required with Go
+// channels. This should be able to be solved by adding an overload wrapper that discards the ok
+// bool.
+
+func (c *Channel[T]) Receive() (T, bool) {
+	if c == nil {
+		// Block forever
+		for {
+		}
+	}
+	v, ok := <-c.ch
+	return v, ok
+}
+
+// c.Close()
+//
+// is equivalent to:
+//
+// close(c)
+func (c *Channel[T]) Close() {
+	if c == nil {
+		panic("close of nil channel")
+	}
+	close(c.ch)
+}
+
+// v := c.ReceiveDiscardOk
+//
+// is equivalent to:
+// v := c<-
+func (c *Channel[T]) ReceiveDiscardOk() T {
+	return_val, _ := c.Receive()
+	return return_val
+}
+
+// c.Len()
+//
+// is equivalent to:
+// len(c)
+//
+// This might not be worth specifying since it is hard to make good use of channel length
+// semantics.
+func (c *Channel[T]) Len() int {
+	if c == nil {
+		return 0
+	}
+	return len(c.ch)
+}
+
+// c.Cap()
+//
+// is equivalent to:
+// cap(c)
+func (c *Channel[T]) Cap() int {
+	if c == nil {
+		return 0
+	}
+	return cap(c.ch)
+}
+
+// c.Iter() returns an iterator that models a for range loop over the channel.
+func (c *Channel[T]) Iter() func(yield func(T) bool) {
+	return func(yield func(T) bool) {
+		for v := range c.ch {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
