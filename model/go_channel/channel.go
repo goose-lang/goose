32fe4c3b27d8234aa03b8c15415229558b2188a8
Tej Chajed <chajed@wisc.edu>
2025-11-07 12:08:12 -0600

    Start Go version of channel.Channel

---
+++ model/go_channel/channel.go
@@ -0,0 +1,4 @@
+// Package go_channel provides the same interface as model/channel, but using the built-in channels as the implementation.
+//
+// This is only used for testing the model - it makes it easy to use the same example with the model and Go channels.
+package go_channel
