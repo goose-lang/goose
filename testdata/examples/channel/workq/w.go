4b162bf788e503923924d25b83becffe6015ad76
Tej Chajed <chajed@wisc.edu>
2026-03-13 16:36:24 -0500

    Remove workqMain

--- testdata/examples/channel/workq/w.go
+++ testdata/examples/channel/workq/w.go
@@ -66,7 +66,6 @@ func wordCount(docs []string) int64 {
 	if len(docs) == 0 {
 		return 0
 	}
-
 	const numWorkers = 2
 	workers := make([]*Worker, numWorkers)
 	for i := range workers {
@@ -93,27 +92,4 @@ func wordCount(docs []string) int64 {
 
 	<-done
 	return total.Load()
-}
-
-func workqMain() {
-	docs := []string{
-		"the cat sat on the mat",
-		"a quick brown fox jumps over the lazy dog",
-		"to be or not to be that is the question",
-		"all that glitters is not gold",
-		"ask not what your country can do for you",
-		"one small step for man one giant leap for mankind",
-		"we hold these truths to be self evident",
-		"in the beginning was the word and the word was good",
-	}
-
-	got := wordCount(docs)
-
-	want := int64(0)
-	for _, doc := range docs {
-		want += int64(len(strings.Fields(doc)))
-	}
-	if got != want {
-		panic("word count: wrong count")
-	}
 }
